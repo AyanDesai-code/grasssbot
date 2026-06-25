f77f2d9344a826609b8899ecdac185e994b50cc3
Ayan Desai <ayan.k.desai@gmail.com>
2026-06-25 18:24:13 -0400

    made a sacrifice to the grass gods

--- app.py
+++ app.py
@@ -14,27 +14,26 @@
 from flask import Flask, request, render_template
 import numpy as np
 import tensorflow as tf
-from PIL import Image 
+from PIL import Image
+import io 
 app = Flask(__name__)
 model = tf.keras.models.load_model("grassmodel.h5")
 
 @app.route("/predict/", methods=["POST"])
 def predict():
-    from PIL import Image
+    image = request.files["imagefile"]
 
-    image = request.files["imagefile"] 
     if image:
-        img_array = tf.keras.utils.img_to_array(image)
+        img_data = image.read()
+        img = Image.open(io.BytesIO(img_data)).convert("RGB")
+        img = img.resize((64, 64))
+        img_array = tf.keras.utils.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0) / 255.0
 
-    processed_img = img_array / 255.0
-    img = Image.open(image.stream)  
-    img = img.resize((64, 64))
-
-    img_array = np.array(img)
-    img_array = np.expand_dims(img_array, axis=0) / 255.0
-    predictions = model.predict(processed_img)
+    predictions = model.predict(img_array)
+    print(model.summary())
+    print(predictions)
     return {"data": predictions.tolist()}
-
 
 
 @app.route("/tester/")
